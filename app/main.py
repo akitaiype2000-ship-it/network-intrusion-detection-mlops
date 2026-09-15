b594398faf0522a98ecbec6c737d56aab1ae0c4d
akitaiype2000-ship-it <akitaiypecherian@gmail.com>
2026-09-15 13:08:48 +0530

    Add Prometheus monitoring

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI, HTTPException
 from app.schemas import PredictionRequest
 from app.predict import predict
 from app.logger import logger
-
+from prometheus_fastapi_instrumentator import Instrumentator
+app = FastAPI(
+    title="Network Intrusion Detection API",
+    version="1.0"
+)
+Instrumentator().instrument(app).expose(app)
 app = FastAPI(
     title="Network Intrusion Detection API",
     description="""
